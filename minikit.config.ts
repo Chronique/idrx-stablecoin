8bac5185c98b33e0bf9adffd3fdfd63f5471b2be
ChroniQue <adhi.chronique@gmail.com>
2025-11-15 17:09:59 +0700

    Refactor minikit.config.ts for better readability

--- minikit.config.ts
+++ minikit.config.ts
@@ -1,18 +1,23 @@
 const ROOT_URL =
   process.env.NEXT_PUBLIC_URL ||
-  (process.env.VERCEL_PROJECT_PRODUCTION_URL ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}` : 'http://localhost:3000');
+  (process.env.VERCEL_PROJECT_PRODUCTION_URL
+    ? `https://${process.env.VERCEL_PROJECT_PRODUCTION_URL}`
+    : "http://localhost:3000");
 
 export const minikitConfig = {
   accountAssociation: {
-    header: "eyJmaWQiOjM0NTk5MywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDExMDkxOGJiRmQ2ODE3NjRhMTU3MDU4MUVBNTI0N2ZmYjk5M0NGYzAifQ",
+    header:
+      "eyJmaWQiOjM0NTk5MywidHlwZSI6ImN1c3RvZHkiLCJrZXkiOiIweDExMDkxOGJiRmQ2ODE3NjRhMTU3MDU4MUVBNTI0N2ZmYjk5M0NGYzAifQ",
     payload: "eyJkb21haW4iOiJpZHJ4LXN0YWJsZWNvaW4udmVyY2VsLmFwcCJ9",
-    signature: "MKikHEiicCTLGxjnPmI08a4YDZYV/kdXcbKj/1r7UntxbtZBMr4LYWZmawNeyhrDCZskGZ7U37fBfT30yEYnbhw="
+    signature:
+      "MKikHEiicCTLGxjnPmI08a4YDZYV/kdXcbKj/1r7UntxbtZBMr4LYWZmawNeyhrDCZskGZ7U37fBfT30yEYnbhw=",
   },
   miniapp: {
     version: "1",
     name: "IDRX",
     subtitle: "rupiah-backed-stablecoin",
-    description: "idrx-is-indonesian-rupiah-backed-stablecoin-regulated-1-1-pegged-fast-global-transactions-24-7-access-and-instant-conversion",
+    description:
+      "idrx-is-indonesian-rupiah-backed-stablecoin-regulated-1-1-pegged-fast-global-transactions-24-7-access-and-instant-conversion",
     screenshotUrls: [`${ROOT_URL}/screenshot-portrait.png`],
     iconUrl: `${ROOT_URL}/idrx-logo-official.png`,
     splashImageUrl: `${ROOT_URL}/idrx-splash.png`,
@@ -24,7 +29,8 @@ export const minikitConfig = {
     heroImageUrl: `${ROOT_URL}/idrx-splash.png`,
     tagline: "Get IDRX",
     ogTitle: "idrx-rupiah-stablecoin",
-    ogDescription: "get-idrx-rupiah-backed-stablecoin-for-seamless-web3-transactions",
+    ogDescription:
+      "get-idrx-rupiah-backed-stablecoin-for-seamless-web3-transactions",
     ogImageUrl: `${ROOT_URL}/idrx-splash.png`,
   },
 } as const;
